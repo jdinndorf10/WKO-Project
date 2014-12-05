6fa8835944560082d3d6d7fe6bf733c8bbe276e2
alexbhanneman <abhanneman10@winona.edu>
2014-12-05 13:48:00 -0600

    j

--- HelpNow.js
+++ HelpNow.js
@@ -9,6 +9,10 @@ function pageLoad(){
 	document.getElementById("specialEvents").onclick = specialEventsF;
 	document.getElementById("thirdParty").onclick = thirdPartyF;
 	document.getElementById("planned").onclick = plannedF;
+	
+	document.getElementById("homeButton").onclick = home;
+	document.getElementById("homeButton").onmouseover = homeButtonHoverF;
+	document.getElementById("homeButton").onmouseout = homeButtonF;
 }
 
 function volunteerF(){
@@ -170,3 +174,27 @@ function thirdPartyF(){
 	document.getElementById("thirdParty").className = "blue";
 	document.getElementById("planned").className = "options";	
 }
+
+function home(){
+	window.location.href = "index.html";
+}
+
+function homeButtonHoverF(){
+	document.getElementById("homeButton").id = "homeButtonHover";
+}
+
+function homeButtonF(){
+	document.getElementById("homeButtonHover").id = "homeButton";
+}
+
+$(document).ready(function(){  
+	$('#contact').click(function () {
+		alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel porta metus. Donec pellentesque risus metus, quis lobortis ex elementum at. Proin ut arcu urna. Curabitur facilisis imperdiet velit eget dapibus. Mauris facilisis tristique eros eu pellentesque. Ut ut congue est. Proin tristique neque id nulla vehicula, nec tempus quam aliquam. Sed vitae blandit neque, at volutpat urna. Duis aliquam tempus turpis et hendrerit. Etiam felis metus, venenatis ac tincidunt eget, ullamcorper a dui. In sodales quam sed maximus gravida.');});});
+
+$(document).ready(function(){ 
+	$('#tos').click(function () {
+		alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel porta metus. Donec pellentesque risus metus, quis lobortis ex elementum at. Proin ut arcu urna. Curabitur facilisis imperdiet velit eget dapibus. Mauris facilisis tristique eros eu pellentesque. Ut ut congue est. Proin tristique neque id nulla vehicula, nec tempus quam aliquam. Sed vitae blandit neque, at volutpat urna. Duis aliquam tempus turpis et hendrerit. Etiam felis metus, venenatis ac tincidunt eget, ullamcorper a dui. In sodales quam sed maximus gravida.');});});
+
+$(document).ready(function(){ 
+	$('#priv').click(function () {
+		alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel porta metus. Donec pellentesque risus metus, quis lobortis ex elementum at. Proin ut arcu urna. Curabitur facilisis imperdiet velit eget dapibus. Mauris facilisis tristique eros eu pellentesque. Ut ut congue est. Proin tristique neque id nulla vehicula, nec tempus quam aliquam. Sed vitae blandit neque, at volutpat urna. Duis aliquam tempus turpis et hendrerit. Etiam felis metus, venenatis ac tincidunt eget, ullamcorper a dui. In sodales quam sed maximus gravida.');});});
