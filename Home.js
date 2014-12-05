a777c9adb84eabaaa33b87359f1443347dba0ce8
alexbhanneman <abhanneman10@winona.edu>
2014-12-05 12:55:01 -0600

    home button changes still..

--- Home.js
+++ Home.js
@@ -6,7 +6,13 @@ function pageLoad(){
 
 function home(){
 	window.location.href = "index.html";
+	document.getElementById("homeButton").onmouseover = homeButtonHoverF;
 }
+
+function homeButtonHoverF(){
+	document.getElementById("homeButton").style.backgroundColor = "white";
+}
+
 $(document).ready(function(){  
 	$('#contact').click(function () {
 		alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel porta metus. Donec pellentesque risus metus, quis lobortis ex elementum at. Proin ut arcu urna. Curabitur facilisis imperdiet velit eget dapibus. Mauris facilisis tristique eros eu pellentesque. Ut ut congue est. Proin tristique neque id nulla vehicula, nec tempus quam aliquam. Sed vitae blandit neque, at volutpat urna. Duis aliquam tempus turpis et hendrerit. Etiam felis metus, venenatis ac tincidunt eget, ullamcorper a dui. In sodales quam sed maximus gravida.');});});
